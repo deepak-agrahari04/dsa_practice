5f686e4e560c78e6b7468d28ba6413cbe40297ca
deepak04agrahari <deepak04agrahari@gmail.com>
2026-04-13 23:18:00 +0530

    add 19

---
+++ Linked-List/19.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+      /*  ListNode temp = head;
+       int len= 0;
+       int pos;
+       while(temp != null){
+        len++;
+        temp = temp.next;
+       } 
+       if(len == n) return head.next;
+       pos = len-n;
+       temp = head;
+       for(int i=1;i<pos;i++){
+            temp = temp.next;
+       }
+       temp.next = temp.next.next;
+       return head; */
+
+       ListNode f = head;
+       ListNode s = head;
+       
+       for(int i = 0;i<n;i++){
+        f = f.next;
+       }
+       if(f == null) return head.next;
+       while(f.next != null){
+        f = f.next;
+        s = s.next;
+       }
+       s.next = s.next.next;
+       return head;
+    }
+    
+}
